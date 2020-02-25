47b10e10fbefe8fc6f945c857c9cfdab10d33ef6
Joao Pedras <joao@pedras.org>
2020-02-25 03:05:14 -0800

    3328pc code with pdi_boot script

--- include/config_distro_bootcmd.h
+++ include/config_distro_bootcmd.h
@@ -327,6 +327,17 @@
 	"boot_script_dhcp=boot.scr.uimg\0" \
 	BOOTENV_BOOT_TARGETS \
 	\
+	"bootdelay=1\0" \
+	"pdi_boot="							\
+		"if test -e mmc ${config_part} boot_b; then setenv boot_from ${image_b_part}; else if test -e mmc ${config_part} boot_a; then setenv boot_from ${image_a_part}; fi; fi; if test -e mmc ${config_part} nextboot_a; then if test ! -e mmc ${config_part} nextboot_a_done; then setenv boot_from ${image_a_part}; fatwrite mmc ${config_part} 0x00500000 nextboot_a_done 1; fi; else if test -e mmc ${config_part} nextboot_b; then if test ! -e mmc ${config_part} nextboot_b_done; then setenv boot_from ${image_b_part}; fatwrite mmc ${config_part} 0x00500000 nextboot_b_done 1; fi; fi; fi; run pdi_boot_custom_extlinux\0"					\
+	"config_part="							\
+       		"1:8\0"							\
+	"image_a_part="							\
+		"1:4\0"							\
+	"image_b_part="							\
+       		"1:6\0"							\
+	"pdi_boot_custom_extlinux="						\
+       		"echo Booting from ${boot_from}; sysboot mmc ${boot_from} any 0x00500000 /extlinux/extlinux.conf\0" \
 	"boot_extlinux="                                                  \
 		"sysboot ${devtype} ${devnum}:${distro_bootpart} any "    \
 			"${scriptaddr} ${prefix}extlinux/extlinux.conf\0" \
@@ -386,7 +397,7 @@
 		"done\0"
 
 #ifndef CONFIG_BOOTCOMMAND
-#define CONFIG_BOOTCOMMAND "run distro_bootcmd"
+#define CONFIG_BOOTCOMMAND "run pdi_boot"
 #endif
 
 #endif  /* _CONFIG_CMD_DISTRO_BOOTCMD_H */
